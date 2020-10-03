6c520155f0f87919de0c6854392f424368f5b012
yum650350 <yum650350@gmail.com>
2020-10-03 22:43:38 +0800

    create android fetchDeferredUrl method

--- android/src/main/java/com/reactlibrary/FacebookAppLinkModule.java
+++ android/src/main/java/com/reactlibrary/FacebookAppLinkModule.java
@@ -1,9 +1,13 @@
 package com.reactlibrary;
 
+import com.facebook.applinks.AppLinkData;
+import com.facebook.FacebookSdk;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 
 public class FacebookAppLinkModule extends ReactContextBaseJavaModule {
 
@@ -20,8 +24,23 @@ public class FacebookAppLinkModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void fetchUrl(final Promise promise) {
+        try {
+            FacebookSdk.setAutoInitEnabled(true);
+            FacebookSdk.fullyInitialize();
+            AppLinkData.fetchDeferredAppLinkData(this.reactContext.getApplicationContext(),
+                new AppLinkData.CompletionHandler() {
+                @Override
+                public void onDeferredAppLinkDataFetched(AppLinkData appLinkData) {
+                    if (appLinkData != null){
+                        promise.resolve(appLinkData.getTargetUri().toString());
+                    } else {
+                        promise.resolve(null);
+                    }
+                }
+            });
+        } catch (Exception e) {
+            promise.reject("error", e.getMessage(), e);
+        }
     }
 }
